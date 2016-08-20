a827bfad99de7b23cda1e865d55b5867d00e7a0b
ulif <uli@gnufix.de>
2016-08-20 11:17:21 +0200

    Add tests.

---
+++ test_pyjamendo.py
@@ -0,0 +1,10 @@
+# Tests for pyjamendo
+# Requires py.test
+from pyjamendo import jam_radios_to_m3u
+
+def test_jam_radios_to_m3u(capfd):
+    jam_radios_to_m3u()
+    out, err = capfd.readouterr()
+    assert out.startswith("#EXTM3U")
+    assert "EXTINF:-1, " in out
+    assert "https" not in out   # how bad, we can test this
