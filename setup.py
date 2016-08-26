e25842d26bac95332584262b4723cd2326b279e3
ulif <uli@gnufix.de>
2016-08-27 01:51:46 +0200

    Add a setup.py.
    Okay, this starts to become overkill, but we cannot easily do any
    serious automatic processing of tests etc., if there is no
    setup.py. `tox` wants it, `travis.ci` wants it, even I want it. It
    helps to get the dependencies and all right.

---
+++ setup.py
@@ -0,0 +1,66 @@
+import os
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup_requires = [
+    'pytest_runner',
+    ]
+
+install_requires = [
+    'setuptools',
+    ]
+
+tests_require = [
+    'pytest',
+    'pytest-cov',
+    ]
+
+docs_require = [
+    'Sphinx',
+    'sphinx_rtd_theme',
+    ]
+
+setup(
+    name="pyjamendo",
+    version="0.2.dev0",
+    author="Uli Fouquet",
+    author_email="uli@gnufix.de",
+    description=(
+        "Build m3u lists for streaming jamando.com internet radio stations."),
+    license="MIT",
+    keywords="jamendo internetradio m3u extm3u",
+    url="https://github.com/ulif/pyjamendo/",
+    py_modules=['pyjamendo'],
+    packages=[],
+    namespace_packages=[],
+    long_description=read('README.rst') + '\n\n\n' + read('CHANGES.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=dict(
+        tests=tests_require,
+        docs=docs_require,
+        ),
+    entry_points={
+    },
+)
